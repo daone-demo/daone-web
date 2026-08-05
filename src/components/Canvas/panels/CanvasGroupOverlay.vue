cd909e1af70bff5408236b754adf393ea622388b
gengyi <junjunxu92@gmail.com>
2026-08-05 19:36:44 +0800

    feat: update

--- src/components/Canvas/panels/CanvasGroupOverlay.vue
+++ src/components/Canvas/panels/CanvasGroupOverlay.vue
@@ -5,6 +5,11 @@
     :style="overlayStyle"
   >
     <div
+      class="canvas__group-hit"
+      title="点击选中整组"
+      @mousedown.stop="onHitMouseDown"
+    />
+    <div
       class="canvas__group-label"
       title="点击选中整组，拖动可移动"
       @mousedown.stop="onLabelMouseDown"
@@ -49,6 +54,59 @@ const overlayStyle = computed(() => ({
   height: `${props.box.height}px`,
 }))
 
+function cloneMouseEvent(type: 'mousedown' | 'mouseup' | 'click', source: MouseEvent) {
+  return new MouseEvent(type, {
+    bubbles: true,
+    cancelable: true,
+    view: window,
+    detail: type === 'click' ? 1 : source.detail,
+    clientX: source.clientX,
+    clientY: source.clientY,
+    screenX: source.screenX,
+    screenY: source.screenY,
+    button: source.button,
+    buttons: source.buttons,
+    ctrlKey: source.ctrlKey,
+    shiftKey: source.shiftKey,
+    altKey: source.altKey,
+    metaKey: source.metaKey,
+  })
+}
+
+function passPointerToGraphTarget(event: MouseEvent, hitEl: HTMLElement): boolean {
+  hitEl.style.pointerEvents = 'none'
+  const target = document.elementFromPoint(event.clientX, event.clientY)
+  hitEl.style.pointerEvents = 'auto'
+  if (!target) return false
+
+  const graphTarget = target.closest('.x6-node, .x6-edge')
+  if (!graphTarget) return false
+
+  graphTarget.dispatchEvent(cloneMouseEvent('mousedown', event))
+
+  const onMouseUp = (upEvent: MouseEvent) => {
+    hitEl.style.pointerEvents = 'none'
+    const upTarget = document.elementFromPoint(upEvent.clientX, upEvent.clientY)
+    hitEl.style.pointerEvents = 'auto'
+    const graphUp = upTarget?.closest('.x6-node, .x6-edge') ?? graphTarget
+    graphUp.dispatchEvent(cloneMouseEvent('mouseup', upEvent))
+    graphUp.dispatchEvent(cloneMouseEvent('click', upEvent))
+    window.removeEventListener('mouseup', onMouseUp)
+  }
+  window.addEventListener('mouseup', onMouseUp)
+
+  return true
+}
+
+function onHitMouseDown(event: MouseEvent) {
+  if (event.button !== 0) return
+
+  const hitEl = event.currentTarget as HTMLElement
+  if (passPointerToGraphTarget(event, hitEl)) return
+
+  emit('select-group', props.groupId)
+}
+
 function onLabelMouseDown(event: MouseEvent) {
   if (event.button !== 0) return
   emit('select-group', props.groupId)
@@ -57,6 +115,7 @@ function onLabelMouseDown(event: MouseEvent) {
 
 function onHandleMouseDown(event: MouseEvent, handle: GroupResizeHandle) {
   if (event.button !== 0) return
+  emit('select-group', props.groupId)
   emit('resize-start', { event, handle, groupId: props.groupId })
 }
 </script>
