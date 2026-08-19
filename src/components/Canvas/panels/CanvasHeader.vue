6f047cafbb79478011d56be4c49eb056a6a9860c
gengyi <junjunxu92@gmail.com>
2026-08-19 21:40:18 +0800

    feat: 修复p1 bug

--- src/components/Canvas/panels/CanvasHeader.vue
+++ src/components/Canvas/panels/CanvasHeader.vue
@@ -193,7 +193,7 @@
           <div class="canvas__user-menu-balance">
             <span class="canvas__user-menu-balance-left">
               <!-- <span class="canvas__user-menu-balance-icon" aria-hidden="true" /> -->
-              <i class="iconfont icon-huiyuanjifen" style="font-size: 18px;"></i>
+              <i class="iconfont icon-huiyuanjifen" style="font-size: 18px;color: rgb(255, 198, 0);"></i>
               <span class="canvas__user-menu-balance-value">{{ userInfoStore?.pointAccount?.available || 0 }}</span>
             </span>
             <button type="button" class="canvas__user-menu-buy" @click="emit('open-combo')">
